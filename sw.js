32f1ff8d08a5294791f130cb6cf0a4c55626213f
xcao65 <xcao65@gatech.edu>
2020-05-04 01:49:38 -0500

    fix: update sw hostname whitelist

--- sw.js
+++ sw.js
@@ -31,8 +31,7 @@ const PRECACHE_LIST = [
 ]
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
-  "huangxuan.me",
-  "yanshuo.io",
+  "tybbscx.com",
   "cdnjs.cloudflare.com"
 ]
 const DEPRECATED_CACHES = ['precache-v1', 'runtime', 'main-precache-v1', 'main-runtime']
