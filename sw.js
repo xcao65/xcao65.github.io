7b78d188591577d1c3cb0896e1eb40955e89a5b2
xcao65 <xcao65@gatech.edu>
2022-02-14 19:24:32 -0800

    upload images

--- sw.js
+++ sw.js
@@ -31,7 +31,7 @@ const PRECACHE_LIST = [
 ]
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
-  "tybbscx.com",
+  "xunlovejia.com",
   "cdnjs.cloudflare.com"
 ]
 const DEPRECATED_CACHES = ['precache-v1', 'runtime', 'main-precache-v1', 'main-runtime']
